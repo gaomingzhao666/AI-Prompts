a2be4a4ee2aa215a9bbb52283fdbc62c4ea2ac94
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-08-13 14:51:11 +0800

    feat: update page content

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -21,7 +21,7 @@
 		class="backdrop-filter-[12px] group inline-flex h-7 -translate-y-4 animate-fade-in items-center justify-between gap-1 rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white opacity-0 transition-all ease-in hover:cursor-pointer hover:bg-white/20 dark:text-black"
 	>
 		<AnimatedShinyText class="inline-flex items-center justify-center">
-			<span>✨ Introducing Svee UI Template</span>
+			<span>✨ Introducing AI Prompts</span>
 			{' '}
 			<ArrowRightIcon
 				class="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
@@ -31,19 +31,15 @@
 	<h1
 		class="-translate-y-4 animate-fade-in text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl"
 	>
-		Svee UI is the new way
+		AI Prompts is a fast Web App
 		<br class="hidden md:block" />
 		{' '}
-		to build landing pages.
+		powered by Svelte
 	</h1>
 	<p
 		class="mb-12 -translate-y-4 animate-fade-in text-balance text-lg tracking-tight text-gray-400 opacity-0 [--animation-delay:400ms] md:text-xl"
 	>
-		Beautifully designed, animated components and templates built with
-		<br class="hidden md:block" />
-		{' '}
-		Tailwind CSS, <a href="https://svelte.dev" class="underline underline-offset-2">Svelte</a>, and
-		<a href="https://animation-svelte.vercel.app" class="underline underline-offset-2">Svelte Animations</a>.
+		Get optimized prompts for any AI tools and use cases
 	</p>
 	<Button
 		class="-translate-y-4 animate-fade-in gap-1 rounded-lg text-white opacity-0 ease-in-out [--animation-delay:600ms] dark:text-black"
