678249087a2ace0c1b0979beacbd4e74375eeb97
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-08-14 12:14:11 +0800

    feat: complete Footer page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,7 +3,6 @@
 	import ClientSection from '$lib/components/landing/ClientSection.svelte';
 	import CtaSection from '$lib/components/landing/CtaSection.svelte';
 	import HeroSection from '$lib/components/landing/HeroSection.svelte';
-	import PricingSection from '$lib/components/landing/PricingSection.svelte';
 	import SphereMask from '$lib/components/magic/SphereMask/SphereMask.svelte';
 </script>
 
@@ -27,5 +26,4 @@
 <HeroSection />
 <ClientSection />
 <SphereMask />
-<PricingSection />
 <CtaSection />
