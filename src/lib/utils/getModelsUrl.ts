18b52e45ed3b60c34778e38d71e0fbf8caa9aac6
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-08-20 17:13:04 +0800

    fix: wrong variable name

--- src/lib/utils/getModelsUrl.ts
+++ src/lib/utils/getModelsUrl.ts
@@ -2,7 +2,8 @@ export const getModelsUrl = (models: string[]): string[] => {
 	const prefixPath: string = '/src/lib/imgs/';
 	const suffixPath: string = '.svg';
 
-	const modelsPathes: string[] = models.map((data) => prefixPath + data + suffixPath);
+	// models url array
+	const modelsPaths: string[] = models.map((data) => prefixPath + data + suffixPath);
 
-	return modelsPathes;
+	return modelsPaths;
 };
